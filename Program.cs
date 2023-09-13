da707c6ef2b1703291de9bb713bb9f3c3720acc7
Dell-User <Dell-User@DESKTOP-88R8045>
2023-09-13 12:11:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.EXCEPTION_HANDLING
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+
+            {
+                //Int=(ToInt32)/Decimal=(ToDecimal)/Double=(ToDouble)
+                Console.WriteLine("Enter First Number ");
+                Double Num1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Second Number");
+                Double Num2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Your first Number divided by Second Number =R");
+                Console.WriteLine(Num1 / Num2);
+            }
+            catch
+            {
+                Console.Write("Error!!! Code 10110");
+
+            }
+            //Print
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
